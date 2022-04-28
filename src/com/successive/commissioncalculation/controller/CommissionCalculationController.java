37483b743bad7a4e3ee48a9332babad71808ddea
tridentamit <amit.codechef@gmail.com>
2022-04-29 02:34:51 +0530

    controller

---
+++ src/com/successive/commissioncalculation/controller/CommissionCalculationController.java
@@ -0,0 +1,10 @@
+package com.successive.commissioncalculation.controller;
+
+import java.util.List;
+import com.successive.commissioncalculation.model.Trade;
+
+public class CommissionCalculationController {
+  public List<String> getCommissionAmount(final List<Trade> trades){
+    Com
+  }
+}
