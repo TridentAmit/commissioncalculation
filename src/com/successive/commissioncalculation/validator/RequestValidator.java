32d3acdf576a4de17cdc3aaadd369331f1ccdba1
tridentamit <amit.codechef@gmail.com>
2022-04-29 17:05:18 +0530

    Test

--- src/com/successive/commissioncalculation/validator/RequestValidator.java
+++ src/com/successive/commissioncalculation/validator/RequestValidator.java
@@ -9,20 +9,20 @@ import com.successive.commissioncalculation.model.Trade;
  *
  */
 public class RequestValidator {
-  public Trade validateTrade(Trade trade) {
+  public String  validateTrade(Trade trade) {
     if(!("STO".equalsIgnoreCase(trade.getSecurityType()) || "BON".equalsIgnoreCase(trade.getSecurityType()) || "FX".equalsIgnoreCase(trade.getSecurityType()))) {
-      throw new IllegalArgumentException("Not a valid security type");
+      return "Not a valid security type";
     }
     if(!("BUY".equalsIgnoreCase(trade.getTransactionType()) || "SELL".equalsIgnoreCase(trade.getTransactionType()))){
-      throw new IllegalArgumentException("Not a valid transaction type");
+      return "Not a valid transaction type";
     }
     if(trade.getQuantity() < 1) {
-      throw new IllegalArgumentException("Quantity should be more than 0");
+      return "Quantity should be more than 0";
     }
     if(trade.getSecurityPrice() <= 0) {
-      throw new IllegalArgumentException("Price must be more than 0");
+      return "Price must be more than 0";
     }
-    return trade;
+    return null;
   }
 
 }
